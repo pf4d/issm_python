5b4c14d5f22a34830f7720394f6aee5b45a6ad16
Evan Cummings <cummings.evan@gmail.com>
2017-10-07 23:35:28 +0200

    added CSLVR comparison

--- scripts/ISMIP_HOM_A.py
+++ scripts/ISMIP_HOM_A.py
@@ -21,7 +21,7 @@ print '   Constructing Geometry'
 # Define the geometry of the simulation :
 #md = triangle(md, './exp/square.exp', 80000)
 L  = 80000.0
-n  = 20
+n  = 50
 md = squaremesh(md, L, L, n, n)
 md = setmask(md, 'all', '')
 
@@ -41,28 +41,33 @@ md.geometry.thickness = md.geometry.surface - md.geometry.base
 print '   Defining friction parameters'
 
 # one friciton coefficient per node :
-md.friction.coefficient = 200.0 * ones(md.mesh.numberofvertices)
-#md.friction.coefficient[numpy.nonzero(md.mask.groundedice_levelset < 0)[0]] = 0
+md.friction.coefficient = 10 * np.ones(md.mesh.numberofvertices)
+#floating_v = numpy.where(md.mask.groundedice_levelset < 0)[0]
+#md.friction.coefficient[floating_v] = 0
 
 # one friciton exponent (p,q) per element :
 md.friction.p = ones(md.mesh.numberofelements)
-md.friction.q = ones(md.mesh.numberofelements)
+md.friction.q = zeros(md.mesh.numberofelements)
 
 print '   Construct ice rheological properties'
 
 # The rheology parameters sit in the material section :
 
 # B has one value per vertex :
-md.materials.rheology_B = 6.8067e7 * ones(md.mesh.numberofvertices)
+n   = 3.0
+spy = 31556926.0    # s a^{-1}
+A   = 1e-16         # Pa^{-n} s^{-1}
+B   = (A / spy)**(-1/n)
+md.materials.rheology_B = B * ones(md.mesh.numberofvertices)
 
 # n has one value per element :
-md.materials.rheology_n = 3 * ones(md.mesh.numberofelements)
+md.materials.rheology_n = n * ones(md.mesh.numberofelements)
 
 print '   Set boundary conditions'
 
 # Set the default boundary conditions for an ice-sheet :
 md = SetIceSheetBC(md)  # create placeholder arrays for indicies 
-md.extrude(5, 1.0)
+md.extrude(6, 1.0)
 md = setflowequation(md,'HO','all')
 	
 md.stressbalance.spcvx = np.nan * np.ones(md.mesh.numberofvertices)
@@ -74,17 +79,6 @@ basal_v                         = md.mesh.vertexonbase
 md.stressbalance.spcvx[basal_v] = 0.0
 md.stressbalance.spcvy[basal_v] = 0.0
 md.stressbalance.spcvz[basal_v] = 0.0
-
-"""
-  maxY=find(md.mesh.y==max(md.mesh.y) & md.mesh.x~=max(md.mesh.x) & md.mesh.x~=min(md.mesh.x));
-	% create minY
-	%->
-	minY=find(md.mesh.y==min(md.mesh.y) & md.mesh.x~=max(md.mesh.x) & md.mesh.x~=min(md.mesh.x));
-	% set the node that should be paired together
-	% #md.stressbalance.vertex_pairing
-	%->
-	md.stressbalance.vertex_pairing=[minX,maxX;minY,maxY];
-"""
 
 # set periodic lateral boundary conditions :
 minX = np.where(md.mesh.x == 0)[0] + 1
@@ -101,11 +95,9 @@ maxY = np.where(np.logical_and(md.mesh.y == L,
 # set the nodes that should be paired together :
 md.stressbalance.vertex_pairing = np.array([np.append(minX, minY),
                                             np.append(maxX, maxY)]).T
-#md.stressbalance.vertex_pairing = np.vstack((np.vstack((minX+1,maxX+1)).T,
-#                                             np.vstack((minY+1,maxY+1)).T))
 
 # solve :
-md.cluster = generic('name', gethostname(), 'np', 2)
+md.cluster = generic('name', gethostname(), 'np', 1)
 md.verbose = verbose('convergence', True)
 md         = solve(md, 'Stressbalance')
 
@@ -113,9 +105,13 @@ md         = solve(md, 'Stressbalance')
 p   = md.results.StressbalanceSolution.Pressure[md.mesh.vertexonbase]
 u_x = md.results.StressbalanceSolution.Vx[md.mesh.vertexonsurface] 
 u_y = md.results.StressbalanceSolution.Vy[md.mesh.vertexonsurface] 
-u   = (u_x.flatten(), u_y.flatten()) 
+u   = np.array([u_x.flatten(), u_y.flatten()]) 
 
 from fenics_viz      import *
+
+U_lvls = array([u.min(), 10, 20, 30, 40, 50, 60, 70, 80, u.max()])
+
+mdl_odr = 'BP'
 
 tp_kwargs     = {'linestyle'      : '-',
                  'lw'             : 1.0,
@@ -133,14 +129,14 @@ quiver_kwargs = {'pivot'          : 'middle',
 
 plot_variable(u                   = u,
               name                = 'U',
-              direc               = './output/', 
+              direc               = './images/issm/' + mdl_odr + '/', 
               coords              = (md.mesh.x2d, md.mesh.y2d),
               cells               = md.mesh.elements2d - 1,
               figsize             = (8,7),
               cmap                = 'viridis',
               scale               = 'lin',
               numLvls             = 10,
-              levels              = None,
+              levels              = U_lvls,
               levels_2            = None,
               umin                = None,
               umax                = None,
@@ -151,18 +147,17 @@ plot_variable(u                   = u,
               xlabel              = r'$x$',
               ylabel              = r'$y$',
               equal_axes          = True,
-              title               = '',
+              title               = r'$\underline{u} |_S^{\mathrm{ISSM}}$',
               hide_axis           = False,
               colorbar_loc        = 'right',
               contour_type        = 'filled',
               extend              = 'neither',
-              ext                 = '.png',
+              ext                 = '.pdf',
               normalize_vec       = True,
               quiver_kwargs       = quiver_kwargs,
               res                 = 150,
               cb                  = True,
-              cb_format           = '%.1e')
-
+              cb_format           = '%g')
 
 
 
---
+++ scripts/ISMIP_HOM_A_cslvr.py
@@ -0,0 +1,169 @@
+from cslvr       import *
+from fenics_viz  import *
+
+a     = 0.5 * pi / 180     # surface slope in radians
+L     = 80000.0            # width of domain (also 8000, 10000, 14000)
+
+# create a genreic box mesh, we'll fit it to geometry below :
+p1    = Point(0.0, 0.0, 0.0)          # origin
+p2    = Point(L,   L,   1)            # x, y, z corner 
+#mesh  = BoxMesh(p1, p2, 19, 19, 5)    # a box to fill the void 
+mesh  = BoxMesh(p1, p2, 49, 49, 5)    # a box to fill the void 
+
+# output directiories :
+mdl_odr = 'BP'
+out_dir = './results/cslvr/' + mdl_odr + '/'
+plt_dir = './images/cslvr/' + mdl_odr + '/'
+
+# we have a three-dimensional problem here, with periodic lateral boundaries :
+model = D3Model(mesh, out_dir = out_dir, use_periodic = True)
+
+# the ISMIP-HOM experiment A geometry :
+surface = Expression('- x[0] * tan(a)', a=a,
+                     element=model.Q.ufl_element())
+bed     = Expression(  '- x[0] * tan(a) - 1000.0 + 500.0 * ' \
+                     + ' sin(2*pi*x[0]/L) * sin(2*pi*x[1]/L)',
+                     a=a, L=L, element=model.Q.ufl_element())
+
+# mark the exterior facets and interior cells appropriately :
+model.calculate_boundaries()
+
+# deform the mesh to match our desired geometry :
+model.deform_mesh_to_geometry(surface, bed)
+
+# initialize all the pertinent variables :
+model.init_beta(1e16)             # friction coefficient for no-slip bed
+model.init_A(1e-16)               # isothermal flow-rate factor
+
+# we can choose any of these to solve our 3D-momentum problem :
+if mdl_odr == 'BP':
+  mom = MomentumBP(model)
+elif mdl_odr == 'BP_duk':
+  mom = MomentumDukowiczBP(model)
+elif mdl_odr == 'RS':
+  mom = MomentumDukowiczStokesReduced(model)
+elif mdl_odr == 'FS_duk':
+  mom = MomentumDukowiczStokes(model)
+elif mdl_odr == 'FS_stab':
+  mom = MomentumNitscheStokes(model, stabilized=True)
+elif mdl_odr == 'FS_th':
+  mom = MomentumNitscheStokes(model, stabilized=False)
+mom.solve_params['solve_vert_velocity'] = False
+mom.solve_params['solver']['newton_solver']['relative_tolerance'] = 5e-15
+mom.solve_params['solver']['newton_solver']['maximum_iterations'] = 60
+mom.solve()
+
+# let's investigate the velocity divergence :
+divU = project(div(model.U3))
+
+# the purpose for everything below this line is data visualization :
+#===============================================================================
+
+# save these files with a name that makes sense for use with paraview :
+model.save_xdmf(model.p,  'p')
+model.save_xdmf(model.U3, 'U')
+model.save_xdmf(divU,     'divU')
+
+# create the bed and surface meshes :
+model.form_bed_mesh()
+model.form_srf_mesh()
+
+# create 2D models :
+bedmodel = D2Model(model.bedmesh, out_dir)
+srfmodel = D2Model(model.srfmesh, out_dir)
+
+# we don't have a function for this included in the `model' instance, 
+# so we have to make one ourselves :
+divU_b   = Function(bedmodel.Q, name='divU')
+
+# function allows Lagrange interpolation between different meshes :
+bedmodel.assign_submesh_variable(divU_b, divU)
+srfmodel.assign_submesh_variable(srfmodel.U3, model.U3)
+srfmodel.init_U_mag(srfmodel.U3)  # calculates the velocity magnitude 
+bedmodel.assign_submesh_variable(bedmodel.p,  model.p)
+
+# figure out some nice-looking contour levels :
+U_min  = srfmodel.U_mag.vector().min()
+U_max  = srfmodel.U_mag.vector().max()
+U_lvls = array([U_min, 10, 20, 30, 40, 50, 60, 70, 80, U_max])
+
+p_min  = bedmodel.p.vector().min()
+p_max  = bedmodel.p.vector().max()
+p_lvls = array([4e6, 5e6, 6e6, 7e6, 8e6, 9e6, 1e7, 1.1e7, 1.2e7, p_max])
+
+d_min  = divU_b.vector().min()
+d_max  = divU_b.vector().max()
+d_lvls = array([d_min, -5e-3, -2.5e-3, -1e-3, 
+                1e-3, 2.5e-3, 5e-3, d_max])
+
+tp_kwargs     = {'linestyle'      : '-',
+                 'lw'             : 1.0,
+                 'color'          : 'k',
+                 'alpha'          : 0.5}
+
+quiver_kwargs = {'pivot'          : 'middle',
+                 'color'          : 'k',
+                 'scale'          : None,
+                 'alpha'          : 0.8,
+                 'width'          : 0.005,
+                 'headwidth'      : 3.0, 
+                 'headlength'     : 3.0, 
+                 'headaxislength' : 3.0}
+
+# these functions allow the plotting of an arbitrary FEniCS function or 
+# vector that reside on a two-dimensional mesh (hence the D2Model
+# instantiations above.
+plot_variable(u                   = srfmodel.U3,
+              name                = 'U',
+              coords              = None,
+              cells               = None,
+              direc               = plt_dir, 
+              figsize             = (8,7),
+              cmap                = 'viridis',
+              scale               = 'lin',
+              numLvls             = 10,
+              levels              = U_lvls,
+              levels_2            = None,
+              umin                = None,
+              umax                = None,
+              tp                  = True,
+              tp_kwargs           = tp_kwargs,
+              show                = False,
+              hide_ax_tick_labels = False,
+              xlabel              = r'$x$',
+              ylabel              = r'$y$',
+              equal_axes          = True,
+              title               = r'$\underline{u} |_S^{\mathrm{CSLVR}}$',
+              hide_axis           = False,
+              colorbar_loc        = 'right',
+              contour_type        = 'filled',
+              extend              = 'neither',
+              ext                 = '.pdf',
+              normalize_vec       = True,
+              quiver_kwargs       = quiver_kwargs,
+              res                 = 150,
+              cb                  = True,
+              cb_format           = '%g')
+
+plot_variable(u = bedmodel.p, name = 'p', direc = plt_dir,
+              ext                 = '.pdf',
+              title               = r'$p |_B$',
+              levels              = p_lvls,
+              cmap                = 'viridis',
+              tp                  = True,
+              show                = False,
+              extend              = 'min',
+              cb_format           = '%.1e')
+
+plot_variable(u = divU_b, name = 'divU', direc = plt_dir,
+              ext                 = '.pdf',
+              title               = r'$\nabla \cdot \underline{u} |_B$',
+              cmap                = 'RdGy',
+              levels              = None,#d_lvls,
+              tp                  = True,
+              show                = False,
+              extend              = 'neither',
+              cb_format           = '%.1e')
+
+
+
