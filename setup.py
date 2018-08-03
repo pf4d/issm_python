7d9067d7d7fb53b3a6643f4906eb2543a924e290
pf4d <cummings.evan@gmail.com>
2018-08-03 10:05:39 +0200

    minor version number change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(  name        = 'issm',
-        version     = '14.2',
+        version     = '4.12',
         description = 'Ice Sheet System Model',
         author      = 'UCI/JPL modified by Evan Cummings',
         url         = 'https://github.com/pf4d/issm_python',
